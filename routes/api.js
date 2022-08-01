24e483016548527b88db52f82dfd226577906c5d
yeahyouknowme <>
2022-07-31 18:33:09 -0700

    stringified

--- routes/api.js
+++ routes/api.js
@@ -143,7 +143,7 @@ module.exports = function apiRoutes(app, dynamodb) {
             if (!comment)
                 res.json('missing required field "Comment"');
             if (err) {
-                res.json('error: ' + err + ' \n' + req.body.comment);
+                res.json('error: ' + err + ' \n' + JSON.stringify(req.body));
             }
             else {
                 res.send({
